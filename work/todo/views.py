8bc6efa0a04238e21a7c0e0cbf8e9c279040c579
vaibhavRaj7667 <zivaibhav1@gmail.com>
2024-09-25 16:43:54 +0530

    create_user

--- work/todo/views.py
+++ work/todo/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render,redirect
 from .forms import Task_form
 from .models import Todo
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login , logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth.models import User
+
+
+def logout_view(request):
+    if request.method=="POST":
+        logout(request)
+        return redirect('home')
 
 
 def create_user(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"user alredy in DB")
+            elif User.objects.filter(email=email).exists():
+                messages.error(request,"email alredy in DB")
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password1)
+                user.save()
+                messages.success(request,"user created succesfully")
+                return redirect('home')
+            
+        else:
+             messages.error(request, 'Passwords do not match.')
+
     return render(request,'create_user.html')
 
 
@@ -28,7 +54,7 @@ def home(request):
 
 @login_required
 def create_task(request):
-    all_data = Todo.objects.all()
+    all_data = Todo.objects.filter(user=request.user)
     if request.method=='POST':
         form = Task_form(request.POST)
         if form.is_valid():
--- work/work/urls.py
+++ work/work/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from todo import views 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('create/',views.create_task,name='create_task'),
     path('newuser/',views.create_user,name='create_user'),
     path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
+   
 ]
