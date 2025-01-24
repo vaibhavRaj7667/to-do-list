d4d6ec2ee8d499a0839dfeabc84435e04e2ac9d2
vaibhavRaj7667 <zivaibhav1@gmail.com>
2025-01-24 14:26:22 +0530

    host

--- work/work/settings.py
+++ work/work/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&!whlng*)i3gvh)a*@(mws06+4lcsh53u)mcx%#k+p7hg+77^k
 DEBUG = False
 SECRET_KEY = os.getenv('SECRET_KEY', '7667387499')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = False
 
 
 # Application definition
